19aeeae75ae2d5672b406edf2d83360be495c97b
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-09-11 17:01:03 -0700

    Added stub code for coin_change

--- dp/coin_change.py
+++ dp/coin_change.py
@@ -4,3 +4,14 @@ __author__ = 'sarangis'
 https://www.hackerrank.com/challenges/coin-change
 '''
 
+def coin_change(total_sum, coin_list):
+    return 0
+
+def main():
+    total_sum = 4
+    coin_list = [1,2,3]
+    num_possibilities = coin_change(total_sum, coin_list)
+    print(num_possibilities)
+
+if __name__ == "__main__":
+    main()
