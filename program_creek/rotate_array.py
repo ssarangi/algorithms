1f3498ac25c362a931ea308a2e41a2e10c7afd55
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-10-05 14:29:18 -0700

    Working inplace rotation. have to do the bubble rotate

--- program_creek/rotate_array.py
+++ program_creek/rotate_array.py
@@ -26,6 +26,33 @@ THE SOFTWARE.
 # Bubble Rotate
 # Reversal
 
+# python -m unittest rotate_array.py
+
+import sys, logging
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+
+def reverse(arr, start, end):
+    # if (end - start == 1):
+    #     arr[start], arr[end] = arr[end], arr[start]
+    #     return arr
+    #
+    # center = int((start + end + 1) / 2)
+    # for i in range(start, center):
+    #     arr[i], arr[end - i - 1] = arr[end - i - 1], arr[i]
+
+    j = end
+    for i in range(start, end):
+        if i >= j:
+            break
+        arr[i], arr[j] = arr[j], arr[i]
+        j -= 1
+
+    return arr
+
 def rotate_arr_intermediate_array(arr, k):
     result = []
     for i in range(k + 1, len(arr)):
@@ -38,14 +65,26 @@ def rotate_arr_intermediate_array(arr, k):
 
 def bubble_rotate(arr, k):
     result = []
-    for i in range()
+    # for i in range()
+
+def inplace_rotate(arr, k):
+    border = len(arr) - k - 1
+
+    arr = reverse(arr, 0, border)
+    arr = reverse(arr, border + 1, len(arr) - 1)
+    arr = reverse(arr, 0, len(arr) - 1)
+    return arr
 
 import unittest
 
 class Test(unittest.TestCase):
     def test(self):
         self.assertEqual(rotate_arr_intermediate_array([1,2,3,4,5,6,7], k=3), [5,6,7,1,2,3,4])
-        self.assertEqual(bubble_rotate([1,2,3,4,5,6,7], k=3), [5,6,7,1,2,3,4])
+        # self.assertEqual(bubble_rotate([1,2,3,4,5,6,7], k=3), [5,6,7,1,2,3,4])
+        self.assertEqual(inplace_rotate([1,2,3,4,5,6,7], k=3), [5,6,7,1,2,3,4])
+        self.assertEqual(inplace_rotate([1,2,3,4,5,6,7], k=2), [6,7,1,2,3,4,5])
 
 if __name__ == "__main__":
-    unittest.main()
+    arr = [1,2,3,4,5,6,7]
+    print(inplace_rotate(arr, k=2))
+    # unittest.main()
