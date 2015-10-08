0eab785ef74d628fa43abfee2eb7246c0168e83e
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-10-07 21:18:18 -0700

    Added working trie for incremental search

--- misc/incremental_search.py
+++ misc/incremental_search.py
@@ -28,9 +28,14 @@ tries, suffix trees etc
 """
 
 class Node:
-    def __init__(self, char):
+    def __init__(self, char, parent):
         self._char = char
         self._child_nodes = [None] * 26
+        self._parent = parent
+
+    @property
+    def parent(self):
+        return self._parent
 
     @property
     def char(self):
@@ -53,20 +58,52 @@ def get_character(ascii):
 
 def trie(word, root):
     nodes = root
+    parent = None
     for c in word:
         ascii = get_ascii(c)
         current_node = nodes[ascii]
 
         if current_node == None:
-            current_node = Node(c)
+            current_node = Node(c, parent)
             nodes[ascii] = current_node
 
         nodes = current_node.children
+        parent = current_node
 
     return root
 
+def get_suffixes(node, result, curr_string):
+    is_leaf = True
+    for child in node.children:
+        if child is not None:
+            is_leaf = False
+            new_str = curr_string + child.char
+            get_suffixes(child, result, new_str)
+
+    if is_leaf:
+        result.append(curr_string)
+
+def incremental_search_trie(string, root):
+    results = []
+    nodes = root
+    print(nodes)
+    current_node = None
+    for c in string:
+        ascii = get_ascii(c)
+        current_node = nodes[ascii]
+
+    get_suffixes(current_node, results, "")
+
+    new_results = []
+    for result in results:
+        new_results.append(string + result)
+
+    return new_results
+
+
 def create_trie(words, root):
     for word in words:
+        word = word.rstrip('\n')
         root = trie(word, root)
 
     return root
@@ -74,16 +111,22 @@ def create_trie(words, root):
 def read_dictionary(filename):
     root = []
     for i in range(0, 26):
-        root.append(Node(chr(i + ord('a'))))
+        root.append(Node(chr(i + ord('a')), None))
 
     f = open(filename)
     words = f.readlines()
     f.close()
 
-    create_trie(words, root)
+    root = create_trie(words, root)
+    return root
 
 def main():
-    read_dictionary('wordsEn.txt')
+    root = read_dictionary('wordsEn.txt')
+    string = input("String: ")
+    results = incremental_search_trie(string, root)
+
+    for result in results:
+        print(result)
 
 if __name__ == "__main__":
     main()
