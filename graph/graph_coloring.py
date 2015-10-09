bb95d17e67d84b01821c5032d7434e3ceb20ed0f
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-10-09 16:05:50 -0700

    Add graph coloring stub

---
+++ graph/graph_coloring.py
@@ -0,0 +1,39 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015 <Satyajit Sarangi>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+"""
+Graph coloring problem is to assign colors to certain elements of a graph
+subject to certain constraints.
+
+Vertex coloring is the most common graph coloring problem. The problem is,
+given m colors, find a way of coloring the vertices of a graph such that no
+two adjacent vertices are colored using same color. The other graph coloring
+problems like Edge Coloring (No vertex is incident to two edges of same color)
+and Face Coloring (Geographical Map Coloring) can be transformed into vertex
+coloring.
+Chromatic Number: The smallest number of colors needed to color a graph G is
+called its chromatic number. For example, the following can be colored minimum
+3 colors.
+"""
+
