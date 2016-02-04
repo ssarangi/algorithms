f215feb85109d40d8e2c6609a058ac7f3a210241
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-03 16:31:02 -0800

    Added problem given by minh

---
+++ leetcode/paint_house_II.py
@@ -0,0 +1,38 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015 <Satyajit Sarangi>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+"""
+Problem:
+There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a
+certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is
+the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find
+the minimum cost to paint all houses.
+
+Note:
+All costs are positive integers.
+Follow up:
+Could you solve it in O(nk) runtime?
+"""
+
