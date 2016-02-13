7e6193eba98d12da908deea0d3ba4ec4892aacfa
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-12 18:47:16 -0800

    partial code

--- epi/stacks/print_binary_tree_9_2.py
+++ epi/stacks/print_binary_tree_9_2.py
@@ -1,3 +1,17 @@
+from geeksforgeeks.bst.bst
+
+class Queue:
+    def __init__(self):
+        self.ds = []
+
+    def enqueue(self, v):
+        self.ds.append(v)
+
+    def dequeue(self):
+        val = self.ds[0]
+        self.remove(val)
+        return val
+
 def main():
     pass
 
