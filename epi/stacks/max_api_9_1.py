a007c3efb9ffc6b16fc57a3671f9e9b10a7b9831
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-12 18:27:26 -0800

    added the stack max api problem

--- epi/stacks/max_api_9_1.py
+++ epi/stacks/max_api_9_1.py
@@ -1,27 +1,35 @@
 class Stack:
     def __init__(self):
         self.ds = []
+        self.maxv = None
 
     def push(self, v):
-        self.ds.append(v)
+        if len(self.ds) == 0:
+            self.maxv = v
+
+        self.ds.append((v, max(self.maxv, v)))
 
     def pop(self):
         self.ds.pop()
 
-    def top(self):
+    def peek(self):
         return self.ds[-1]
 
     def empty(self):
         return len(self.ds) == 0
 
     def max(self):
-        pass
+        if self.empty():
+            raise Exception("No elements in the stack")
+
+        return self.ds[-1][1]
 
 def main():
     s = Stack()
-    s.push(4)
-    s.push(1)
     s.push(2)
+    s.push(1)
+    s.push(4)
+    print(s.max())
     s.pop()
     s.push(3)
     
