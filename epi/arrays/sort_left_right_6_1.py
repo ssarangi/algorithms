308eb4d1ae67ecc746c820d7b433de37ff75150e
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-14 11:13:02 -0800

    commited code for sort left right

--- epi/arrays/sort_left_right_6_1.py
+++ epi/arrays/sort_left_right_6_1.py
@@ -1,11 +1,28 @@
 # Use O(1) space complexity and O(n) time complexity
-def sort_left_right(arr, i):
-    for cntr, el in enumerate(arr):
-        if arr[cntr] > arr[i]:
-            pass
+def sort_left_right(arr, pivot):
+    pivot_el = arr[pivot]
+    # First swap the pivot element
+    arr[pivot], arr[len(arr) - 1] = arr[len(arr)-1], arr[pivot]
+
+    # Now start looping through the array to see if we can swap all the elements
+    pivot = len(arr) - 1
+    i = 0
+    j = len(arr) - 2
+
+    while i < j:
+        if arr[i] < pivot_el:
+            i += 1
+        elif arr[j] > pivot_el:
+            j -= 1
+        else:
+            arr[i], arr[j] = arr[j], arr[i]
+
+    # Now restore the pivot element position
+    arr[i], arr[pivot] = arr[pivot], arr[i]
+    return arr
 
 def main():
-    a1 = [3,4,2,5,9,8,0]
+    a1 = [3,6,2,5,9,8,1,0]
     i = 3
     print(sort_left_right(a1, i))
 
