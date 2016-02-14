5fddde01751b1561b9c0c574b52b8362221c25f4
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-14 15:15:42 -0800

    consecutive max all generalized

--- epi/arrays/consecutive_max_6_3.py
+++ epi/arrays/consecutive_max_6_3.py
@@ -6,15 +6,31 @@ def consecutive_max_diff(arr):
     max_v = -sys.maxsize
 
     for i in range(0, len(arr) - 1):
-        new_arr[i+1] = arr[i+1] + arr[i]
-        min_v = min(min_v, new_arr[i+1])
-        max_v = max(max_v, new_arr[i+1])
+        new_arr[i] = arr[i+1] + arr[i]
+        min_v = min(min_v, new_arr[i])
+        max_v = max(max_v, new_arr[i])
 
+    return max_v - min_v
+
+def consecutive_max_diff_k(arr, k):
+    new_arr = [0] * len(arr)
+    min_v = sys.maxsize
+    max_v = -sys.maxsize
+
+    for i in range(0, len(arr) - k):
+        for j in range(0, k):
+            new_arr[i] += arr[i+j]
+
+        min_v = min(min_v, new_arr[i])
+        max_v = max(max_v, new_arr[i])
+
+    print(new_arr)
     return max_v - min_v
 
 def main():
     arr = [1,5,6,7,3,4,9,8]
     print(consecutive_max_diff(arr))
+    print(consecutive_max_diff_k(arr, len(arr) // 2))
 
 if __name__ == "__main__":
     main()
