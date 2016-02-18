72e9fb172e19702706637547d00b60561ef583af
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-17 19:13:42 -0800

    cyclically sorted array

---
+++ epi/searching/cyclically_sorted_12_2.py
@@ -0,0 +1,10 @@
+# Find O(log n) algorithm for finding smallest element
+def cyclically_sorted_search(arr):
+    pass
+
+def main():
+    arr = [378, 478, 550, 631, 103, 203, 220, 234, 279, 368]
+    print(cyclically_sorted_search(arr))
+
+if __name__ == "__main__":
+    main()
