b816e6e680c12dc654bb098f694628f94a1e1ec5
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-18 17:42:07 -0800

    added bst files

--- epi/binary_search_tree/bst_15_1__15_2.py
+++ epi/binary_search_tree/bst_15_1__15_2.py
@@ -1,8 +1,11 @@
+# Problem 15.1 and 15.2
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
+        self.parent = None
 
     def __str__(self):
         return str(self.data)
@@ -26,8 +29,10 @@ class BST:
 
             if v < prev.data:
                 prev.left = Node(v)
+                prev.left.parent = prev
             else:
                 prev.right = Node(v)
+                prev.right.parent = prev
 
     def valid(self, node):
         if node is None:
@@ -39,6 +44,43 @@ class BST:
 
         return self.valid(node.left) and self.valid(node.right)
 
+    def find_key(self, k):
+        cn = self.root
+        while cn is not None and cn.data != k:
+            if k > cn.data:
+                cn = cn.right
+            else:
+                cn = cn.left
+
+        return cn
+
+    def find_next_largest_element(self, k):
+        start_node = self.find_key(k)
+        if start_node is None:
+            return None
+
+        # Find the parent which has a right child
+        parent = start_node.parent
+        level = 1
+        while parent.right is None or parent.right == start_node:
+            parent = parent.parent
+            level += 1
+
+        # Now we found the parent
+        # So drill down to the exact level and get the next element
+        next_el = parent.right
+        node = next_el
+        prev = node
+
+        while node is not None and level > 0:
+            prev = node
+            if node.left is not None:
+                node = node.left
+            else:
+                node = node.right
+            level -= 1
+
+        return prev
 
 def main():
     a1 = [19, 7, 43, 3, 11, 2, 5, 17, 13, 23, 47, 37, 53, 29, 41, 31]
@@ -48,6 +90,7 @@ def main():
         bst.add(elm)
 
     print(bst.valid(bst.root))
+    print(bst.find_next_largest_element(5))
 
 if __name__ == "__main__":
     main()
