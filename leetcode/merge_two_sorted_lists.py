ed54c098bdf255a8cc21e49b24760aa899f2aa96
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-04-03 16:00:38 +0000

    merge 2 sorted lists

--- leetcode/merge_two_sorted_lists.py
+++ leetcode/merge_two_sorted_lists.py
@@ -14,17 +14,42 @@ class Solution(object):
         p1 = l1
         p2 = l2
         
-        while p1 is not None or p2 is not None:
+        # Output Node
+        on = None
+        head = None
+        
+        if l1 is None and l2 is not None:
+            head = l2
+        elif l1 is not None and l2 is None:
+            head = l1
+        
+        while p1 is not None and p2 is not None:
             if p1.val < p2.val:
                 # Store the value of p1 next
                 p1nxt = p1.next
                 
                 # Arrange p1 -> p2
                 p1.next = p2
+                cn = p1
+                p1 = p1nxt
             else:
+                # Store the value of p2 next
                 p2nxt = p2.next
-                p2.next = p1
                 
+                # Arrange p2 -> p1
+                p2.next = p1
+                cn = p2
+                
+                p2 = p2nxt
+            
+            if on is not None:
+                on.next = cn
+            else:
+                head = cn
+                
+            on = cn
+            
+        return head
 
 def create_list(*args):
     head = None
@@ -51,7 +76,9 @@ def print_list(head):
     
 soln = Solution()
 
-l1 = create_list(1, 3, 5, 7, 9)
-l2 = create_list(2, 4, 6, 8, 10)
+# l1 = create_list(1, 3, 5, 7, 9)
+# l2 = create_list(2, 4, 6, 8, 10, 12, 14, 16, 18)
 
-soln.mergeTwoLists(l1, l2)
+l1 = create_list(1)
+l2 = create_list(2)
+print_list(soln.mergeTwoLists(l1, l2))
