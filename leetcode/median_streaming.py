ed4a8870faa9b87908efd1d437c06aa1e048c106
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-04-04 17:39:17 -0700

    Fixed the median streaming problem. EPI has a problem with the solution.

--- leetcode/median_streaming.py
+++ leetcode/median_streaming.py
@@ -1,17 +1,48 @@
 import heapq
 
+MIN_HEAP = 1
+MAX_HEAP = -1
+
+class PriorityQueue:
+    def __init__(self, heap_type):
+        self.heap_type = heap_type
+        self.queue = []
+
+    def push(self, item, priority = None):
+        if priority is None:
+            sign = 0
+            if self.heap_type == MIN_HEAP:
+                sign = 1
+            else:
+                sign = -1
+            priority = sign * item
+
+        heapq.heappush(self.queue, (priority, item))
+
+    def pop(self):
+        return heapq.heappop(self.queue)[1]
+
+    def top(self):
+        v = heapq.heappop(self.queue)
+        heapq.heappush(self.queue, v)
+        return v[1]
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def size(self):
+        return len(self.queue)
+
+    def __str__(self):
+        return str(self.queue)
+
 class MedianFinder:
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.min_heap = []
-        self.max_heap = []
-        
-    def top(self, heap):
-        val = heapq.heappop(heap)
-        heapq.heappush(heap, val)
-        return val
+        self.min_heap = PriorityQueue(MIN_HEAP)
+        self.max_heap = PriorityQueue(MAX_HEAP)
 
     def addNum(self, num):
         """
@@ -20,32 +51,31 @@ class MedianFinder:
         :rtype: void
         """
         # If the min heap is empty then add it to min heap
-        if len(self.min_heap) == 0:
-            heapq.heappush(self.min_heap, num)
+        if self.min_heap.size() == 0:
+            self.min_heap.push(num)
         else:
-            if num > self.top(self.min_heap):
-                heapq.heappush(self.min_heap, num)
+            if num >= self.min_heap.top():
+                self.min_heap.push(num)
             else:
-                heapq.heappush(self.max_heap, num)
-                
-        if len(self.min_heap) > len(self.max_heap) + 1:
-            heapq.heappush(self.max_heap, heapq.heappop(self.min_heap))
-        elif len(self.max_heap) > len(self.min_heap) + 1:
-            heapq.heappush(self.min_heap, heapq.heappop(self.max_heap))
+                self.max_heap.push(num)
+
+        if self.min_heap.size() > self.max_heap.size() + 1:
+            self.max_heap.push(self.min_heap.pop())
+        elif self.max_heap.size() > self.min_heap.size() + 1:
+            self.min_heap.push(self.max_heap.pop())
+
             
     def findMedian(self):
         """
         Returns the median of current data stream
         :rtype: float
         """
-        print(self.min_heap)
-        print(self.max_heap)
-        if len(self.min_heap) == len(self.max_heap):
-            print("I am here")
-            return 0.5 * (heapq.heappop(self.min_heap) + heapq.heappop(self.max_heap))
+        if self.min_heap.size() == self.max_heap.size():
+            return 0.5 * (self.min_heap.top() + self.max_heap.top())
+        elif self.min_heap.size() > self.max_heap.size():
+            return self.min_heap.top()
         else:
-            print("Ok.. this is wierd")
-            return heapq.heappop(self.min_heap)
+            return self.max_heap.top()
 
 # Your MedianFinder object will be instantiated and called as such:
 mf = MedianFinder()
